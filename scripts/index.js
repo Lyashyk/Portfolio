b3afd8ded87e02eaf6e087275fca51c209eea8c4
aleks <aleks1002x@gmail.com>
2018-10-29 13:29:52 +0200

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+$(".interfaceButton").click(function() {
+  $(".portfolio-hiddenContent").toggle("slow", function() {
+    $(".interfaceButton").toggleClass("interfaceButton--visible");
+  });
+});
+
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  nav: true,
+  loop: true,
+  autoplay: true
+});
+
+$(".circlestat").circliful({
+  foregroundColor: "#19bd9a",
+  backgroundColor: "#047378",
+  foregroundBorderWidth: 4,
+  backgroundBorderWidth: 4,
+  animation: 1
+});
